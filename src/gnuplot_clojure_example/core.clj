6e2af57e6d652cd139f573faa77b614b33b558f9
Jim Dunn <james.o.dunn@gmail.com>
2020-11-28 21:22:37 -0800

    simplify

--- src/gnuplot_clojure_example/core.clj
+++ src/gnuplot_clojure_example/core.clj
@@ -4,20 +4,20 @@
 
 (defn example-1 []
   (let [lines (sh/proc "feedgnuplot" "--stream" "trigger" "--domain" "--lines" "--xlen" "5")]
-    (doseq [[x y] (partition 2 (interleave (range 20) (repeatedly #(rand-int 20))))]
-      (sh/feed-from-string lines (str x " " y "\nreplot\n"))
-      (Thread/sleep 500))))
+    (doseq [x (range 20)] (sh/feed-from-string lines (str x " " (rand) "\nreplot\n"))
+           (Thread/sleep 500))))
+
 
 (defn example-2 []
   (let [lines (sh/proc "feedgnuplot" "--stream" "trigger" "--domain" "--lines" "--xlen" "5")]
-    (doseq [[x y1 y2] (map vector (range 20) (repeatedly #(rand-int 20)) (repeatedly #(rand-int 20)))]
-      (sh/feed-from-string lines (str x " " y1 " " y2 "\nreplot\n"))
+    (doseq [x (range 20)]
+      (sh/feed-from-string lines (str x " " (rand-int 10) " " (rand-int 10) "\nreplot\n"))
       (Thread/sleep 500))))
 
 (defn example-3 []
   (let [circles (sh/proc "feedgnuplot" "--stream" "trigger" "--domain" "--circles" "--tuplesizeall" "3" "--title" "chachacha")]
-    (doseq [[x y] (map vector (range 20) (repeatedly #(rand-int 20)))]
-      (sh/feed-from-string circles (str x " " y " 0.5" "\nreplot\n"))
+    (doseq [x (range 10)]
+      (sh/feed-from-string circles (str x " " (rand-int 10) " 0.5" "\nreplot\n"))
       (Thread/sleep 500))))
 
 (defn -main [& args]
